58f10dcac1d18e0d81751549cec34fbd282012fd
Maxi <machugab@gmail.com>
2026-05-11 21:33:33 -0300

    to be continue

--- haskellExercises/diapositiva-7.hs
+++ haskellExercises/diapositiva-7.hs
@@ -4,13 +4,56 @@
 -- type Titulo = String
 -- type Autor = String
 
+data Obra = UnaObra
+  { titulo :: Titulo,
+    autores :: [Autor]
+  }
+
+type Titulo = String
+
+type Autor = String
+
+todasLasObras :: [Obra]
+todasLasObras =
+  [ UnaObra "sandman" ["gaiman"],
+    UnaObra "socorro" ["Elsa Bornemann"],
+    UnaObra "buenos presagios" ["gaiman", "pratchett"]
+  ]
+
 -- 1. Verificar Autoría:
 -- Definir la función 'escribio' que determine si un autor específico escribió una obra con un título determinado.
 -- escribio :: Autor -> Titulo -> [Obra] -> Bool
 
+escribio :: Autor -> Titulo -> [Obra] -> Bool
+escribio _ _ [] = False
+escribio auth tit (x : xs)
+  | tit == titulo x = encontrarAutor x auth
+  | otherwise = escribio auth tit xs
+
+encontrarAutor :: Obra -> Autor -> Bool
+encontrarAutor obra elEscritor = elem elEscritor (autores obra)
+
+escribio' :: Autor -> Titulo -> [Obra] -> Bool
+escribio' unAutor unTitulo todasobras = any (tieneAutorYTitulo' unAutor unTitulo) todasobras
+
+-- ! any (condición) data
+-- ! ej any (1==) [0,1,2,3,4,5]
+
+tieneAutorYTitulo' :: Autor -> Titulo -> Obra -> Bool
+tieneAutorYTitulo' unAutor unTitulo unaObra = titulo unaObra == unTitulo && elem unAutor (autores unaObra)
+
 -- 2. Autores de una Obra:
 -- ¿Quién o quienes escribieron una obra? Implementar 'autoresDe' para obtener la lista de autores de un título.
 -- autoresDe :: Titulo -> [Obra] -> [Autor]
+
+autoresDe :: Titulo -> [Obra] -> [Autor]
+autoresDe unTit lasObras = autores (head (filter (encontrarObra unTit) todasobras))
+
+encontrarObra :: Obra -> Titulo -> Bool
+encontrarObra unaObra unTit = unTit == titulo unaObra
+
+autoresDe' :: Titulo -> [Obra] -> [Autor]
+autoresDe' unTitulo todasobras = autores (head (filter ((== unTitulo) . titulo) todasobras))
 
 -- 3. Obras de un Autor:
 -- ¿Qué obra escribió cierta persona? Implementar 'obrasDe' que devuelva los títulos de las obras de un autor.
