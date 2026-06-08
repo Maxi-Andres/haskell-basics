1b081acb3130abaf455d374ebb6c3056e57f9b9f
Maxi <machugab@gmail.com>
2026-06-08 18:50:04 -0300

    tipo parcial 4

--- haskellExercises/tipoParcial4.hs
+++ haskellExercises/tipoParcial4.hs
@@ -21,14 +21,37 @@ festivales =
 -- a) festivalesEn :: String -> [Festival] -> [String]
 -- Dado un nombre de ciudad, devuelve los nombres de los festivales que se realizan en esa ciudad.
 
+festivalesEn :: String -> [Festival] -> [String]
+festivalesEn nombreCiudad listaFestivales = map nombreFestival (filter ((== nombreCiudad) . ciudad) listaFestivales)
+
 -- b) bandaPresente :: String -> [Festival] -> Bool
 -- Dado el nombre de una banda, indica si toca en al menos uno de los festivales.
+
+bandaPresente :: String -> [Festival] -> Bool
+bandaPresente nombreBanda listaFestivales = any (elem nombreBanda . bandas) listaFestivales
 
 -- c) festivalesConBanda :: String -> [Festival] -> [String]
 -- Dado el nombre de una banda, devuelve los nombres de los festivales donde esa banda toca.
 
+festivalesConBanda :: String -> [Festival] -> [String]
+festivalesConBanda nombreBanda listaFestivales = map nombreFestival (filter (elem nombreBanda . bandas) listaFestivales)
+
 -- d) todasLasBandas :: [Festival] -> [String]
 -- Devuelve la lista de todas las bandas que tocan en al menos un festival (pueden repetirse).
 
+todasLasBandas2 :: [Festival] -> [String]
+todasLasBandas2 [] = []
+todasLasBandas2 (x : xs) = bandas x ++ todasLasBandas2 xs
+
+todasLasBandas :: [Festival] -> [[String]]
+todasLasBandas [] = []
+todasLasBandas (x : xs) = bandas x : todasLasBandas xs
+
 -- e) granFestival :: Festival -> Bool
 -- Un festival es "gran festival" si tiene más de dos bandas. Luego definí grandesFestivales :: [Festival] -> [String] que devuelve los nombres de todos los grandes festivales.
+
+granFestival :: Festival -> Bool
+granFestival festival = length (bandas festival) > 2
+
+grandesFestivales :: [Festival] -> [String]
+grandesFestivales listaFestivales = map nombreFestival (filter (granFestival) listaFestivales)
